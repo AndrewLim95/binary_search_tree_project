97b43ca3a84a8770e3a8723b79fe8da2548bc9c4
Andrew <xemnasq@gmail.com>
2022-08-31 14:56:32 +0800

    Tree class done

---
+++ bst.rb
@@ -0,0 +1,152 @@
+class Node
+    attr_accessor :left, :right, :value
+    def initialize(value)
+        @left = nil
+        @right = nil
+        @value = value
+    end
+end
+
+class Tree
+    attr_accessor :root
+
+    def initialize(arr)
+        arr = arr.sort.uniq
+        last = arr.size - 1
+        @root = build_tree(arr, 0, last)
+    end 
+
+    def build_tree(arr, start, end_index)
+        if (start > end_index)
+            return nil
+        end
+        mid = (start+end_index)/2
+        root = Node.new(arr[mid])
+        root.left = build_tree(arr, start, mid-1)
+        root.right = build_tree(arr, mid+1, end_index)
+        return root
+    end
+
+    def insert(value, node=@root)
+
+        if value < node.value
+            while !node.left.nil?
+                node = node.left
+            end
+            node.left = Node.new(value)
+        elsif value > node.value
+            while !node.right.nil?
+                node = node.right
+            end
+            node.right = Node.new(value)
+        end
+    end
+
+    def delete(value, node=@root)
+        #case 1, removing leaf node
+        #case 2, removing node with 1 child -> point node's parent to node's child
+        #case 3, remove value that has children ->reduce it to case 2 and 1
+        # find min value on right subtree, replace node with min value, remove duplicate on right subtree
+        if node.nil?
+            return nil 
+        elsif value < node.value #loop leftsubtree
+            node.left = delete(value, node.left)
+        elsif value > node.value #loop rightsubtree
+            node.right = delete(value, node.right)
+        else #found target
+            if node.left.nil? && node.right.nil? #delete leaf node
+                node = nil
+            elsif node.left.nil? #delete node with 1 child on left subtree
+                temp = node
+                node = node.right
+                temp = nil
+            elsif node.right.nil? #delete node with 1 child on left subtree
+                temp = node
+                node = node.left
+                temp = nil
+            else #delete node with 2 children, use min value in rightsubtree
+                temp = node.right
+                node.value = temp.value
+                node.right = delete(temp.value,node.right)
+            end
+        end
+        return node
+    end
+
+    def find(value, node=@root)
+        #return node and all its children 
+        if node.nil?
+            return nil 
+        elsif value < node.value
+            find(value, node.left)
+        elsif value > node.value
+            find(value, node.right)
+        else
+            return node.value
+        end
+    end
+#bfs traversal
+    def level_order(node=@root, q=[]) 
+        return node if node.nil?
+        print "#{node.value} "
+        if !node.left.nil?
+            q.push(node.left)
+        end
+        if !node.right.nil?
+            q.push(node.right)
+        end
+        level_order(q.shift, q)
+    end
+
+#dfs traversal : 3ways
+#1) Preorder <root><left><right> 
+#2) Inorder <left><root><right> 
+#3) Postorder <left><right><root>
+
+    def preorder(node=@root)
+        return if node.nil?
+        print"#{node.value} "
+        preorder(node.left)
+        preorder(node.right)
+    end
+
+    def inorder(node=@root)
+        return if node.nil?
+        preorder(node.left)
+        print"#{node.value} "
+        preorder(node.right)
+    end
+
+    def postorder(node=@root)
+        return if node.nil?
+        preorder(node.left)
+        preorder(node.right)
+        print"#{node.value} "
+    end
+
+    def height(node=@root)
+        return -1 if node.nil?
+        left = height(node.left)
+        right = height(node.right)
+        [left,right].max + 1
+    end
+
+    def pretty_print(node = @root, prefix = '', is_left = true)
+        pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+        puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+        pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+    end
+end
+
+
+arr = [1,2,3,4,5,6,7]
+
+tree = Tree.new(arr)
+#     4            2
+#  2     6         1
+#1   3 5   7       0
+puts tree.height(tree.root.right.right)
+
+
+
+
